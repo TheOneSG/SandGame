cb21b0d9e4370d1b8167704be4a1a669052596a4
TheOneSG <99147331+TheOneSG@users.noreply.github.com>
2024-03-05 18:43:58 -0500

    Sliding face works

--- Sand/MicroMixVersion/microMixTemplate/Assets/Team26/Scripts/PlayerWinScreen.cs
+++ Sand/MicroMixVersion/microMixTemplate/Assets/Team26/Scripts/PlayerWinScreen.cs
@@ -15,7 +15,10 @@ public class PlayerWinScreen : MonoBehaviour
     public float winner = 0;
 
     private bool moveEnd = false;
-    private float timer = 0;
+    public float timer = 0;
+
+    public float defaultX = 3f;
+    public float defaultX2 = 2.66f;
 
 
     void Start()
@@ -30,17 +33,20 @@ public class PlayerWinScreen : MonoBehaviour
             Debug.Log("empty Crying");
         }
 
+        
+
         moveEnd = false;
 
         if(winner == 1)
         {
-            Face.transform.position = new Vector3(2.3f, 1.6f, -3);
-            Crying.transform.position = new Vector3(-2.66f, -0.14f, -2);
+            Face.transform.position = new Vector3(defaultX, 1.6f, -3);
+            Crying.transform.position = new Vector3(defaultX2, -0.14f, -2);
         }
 
-        if (winner == 1)
+        if (winner == 2)
         {
-            Face.transform.position = new Vector3(2.3f, 1.6f, -3);
+            Face.transform.position = new Vector3(-defaultX, 1.6f, -3);
+            Crying.transform.position = new Vector3(-defaultX2, -0.14f, -2);
         }
     }
 
@@ -49,9 +55,9 @@ public class PlayerWinScreen : MonoBehaviour
     {
         if(winner == 1)
         {
-            if (moveEnd == false)
+            if (Face.transform.position.x > -defaultX)
             {
-                Face.transform.position += Vector3.left * Time.deltaTime;   
+                Face.transform.position += Vector3.left * Time.deltaTime * 5;   
             }
             else
             {
@@ -61,14 +67,19 @@ public class PlayerWinScreen : MonoBehaviour
             if(moveEnd == true)
             {
                 timer += Time.deltaTime;
-                if(timer > 1){
+                if(timer > 0.5){
+
                     //deactivate child Face.GetChild
+                    Face.transform.GetChild(0).gameObject.SetActive(false);
+
                     //Deactivate child crying.getchild
+                    Crying.transform.GetChild(0).gameObject.SetActive(false);
                 }
 
-                if(timer > 3)
+                if(timer > 2)
                 {
                     //End Scene
+                    Debug.Log("Scene End");
                 }
             }
         }
